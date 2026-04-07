7a69149555ba5b5cc2cd0a2b019151b3c71a7235
Khanh-bit123 <phuc6102006@gmail.com>
2026-04-07 20:38:17 +0700

    fixed

--- AI-THUC-HANH-06/TH07/Phong-làm-sạch-dữ-liệu.py
+++ AI-THUC-HANH-06/TH07/Phong-làm-sạch-dữ-liệu.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+ 
 # 1. Tải dữ liệu
 df = pd.read_csv('pima-indians-diabetes.csv')
 
